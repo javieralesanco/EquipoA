896a1d26312c3efe14e96cd654a9f43874b9768f
Vespertino <Vespertino@C06PC21.informatica.comercio>
2023-01-24 21:41:28 +0100

    24-01-2023

--- PetStore/js/main.js
+++ PetStore/js/main.js
@@ -1,6 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 });
-const createPet = (entrada) => {
+const createPet = (pet) => {
     const abuelo = document.getElementById('main');
+    const top = document.createElement('div');
+    top.classList.add('card');
+    const img = document.createElement('img');
+    (pet.)
+    top.appendChild(img);
+    abuelo.appendChild(top);
+}
+function checkUrl(arrayUrl) {
+    arrayUrl.forEach(url => {
+        if (tryUrl(url))
+            return url;
+    });
+    url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+    if (Math.random() < (1 / 4856))
+        url += 'shiny/';
+    url += Math.floor(Math.random() * 1008);
+    url += '.png';
+    return url;
+}
+// https://pokeapi.co/api/v2/pokemon/{id or name}/
+// sprites, front_default,front_shiny
+function tryUrl(url) {
+    try {
+        new URL(miurl);
+        return true;
+    } catch (err) {
+        return false;
+    }
 }
