812d59fb045a1ecc977cc349480b55e72118353f
Andrei <ftp.andreimiu@gmail.com>
2023-01-27 17:01:38 +0100

    feature/26 nav & footer completed?

--- PetStore/js/menu.js
+++ PetStore/js/menu.js
@@ -1,10 +1,13 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("menu").addEventListener("click", function (event) {
-    //let menu = document.querySelector("menu");
-    let navbar = document.querySelector(".navbar");
-    //menu.classList.toggle("open");
-    //close
-    navbar.classList.toggle("open");
-  });
-});
+
+let menu = document.getElementById("menu");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  navbar.classList.toggle("open");
+  if (menu.textContent == "close") {
+    menu.textContent = "menu";
+  } else {
+    menu.textContent = "close";
+  }
+};
