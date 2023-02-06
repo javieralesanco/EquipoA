82f621eedd2dacdb00610f3bc3505a214f30240b
alexpmAW <alexpezmar3@mail.com>
2023-02-06 17:37:26 +0100

    #21 Mostrar mascota

--- PetStore/js/informacion.js
+++ PetStore/js/informacion.js
@@ -3,8 +3,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const modificar = document.getElementById("Modificar");
     const eliminar = document.getElementById("Borrar");
-    // id = getIdFromUrl();
-    // mostrarMascota(id);  
+    const id = getIdFromUrl();
+    getPet(id);
 
     modificar.addEventListener("click", function () {
         modificarMascota();
@@ -14,26 +14,41 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 });
 
-function mostrarMascota(id) {
-
-
+function mostrarMascota(pet) {
+    const name = document.getElementById('name');
+    const category = document.getElementById('category');
+    const photo = document.getElementById('photo');
+    const tags = document.getElementById('tags');
+    const status = document.getElementById('status');
+    name.textContent += ' : ' + pet.name;
+    category.textContent += ' : ' + pet.category.name;
+    photo.textContent += ' : ' + pet.photoUrls;
+    tags.textContent += ' : ' + pet.tags[0].name;
+    status.textContent += ' : ' + pet.status;
 }
 function modificarMascota(id) {
 
 }
 function eliminarMascota(id) {
     const element = document.querySelector('.informacionAnimales');
-    // fetch(`https://petstore.swagger.io/v2/pet/${id}`, { method: 'DELETE' })
-    //     .then(() => element.innerHTML = 'Delete successful');
-    fetch(`https://petstore.swagger.io/v2/pet/1000`, { method: 'DELETE' })
-        .then(() => element.innerHTML = 'Delete successful');
+    fetch(`https://petstore.swagger.io/v2/pet/${id}`, { method: 'DELETE' })
+        .then(() => element.innerHTML = 'Delete successful')
+        .then(() => element.classList += ' whiteLetters');
+}
 
+function getPet(id) {
+    fetch(`https://petstore.swagger.io/v2/pet/745831`)
+        .then((response) => response.json())
+        .then(pet => {
+            mostrarMascota(pet);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
 
 function getIdFromUrl() {
-
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const id = urlParams.get('id');
-    return id;
+    return urlParams.get('mod');
 }
