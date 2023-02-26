d8d41b1c23d62a4f6ffbe005ccd1122782ceaf2a
Andrei Miu <ftp.andreimiu@gmail.com>
2023-02-26 15:35:56 +0100

    añadir pet

--- PetStore/js/crear.js
+++ PetStore/js/crear.js
@@ -1,67 +1,61 @@
-
-
-
 document.addEventListener("DOMContentLoaded", async function () {
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const form = document.querySelector('#form')
+  const form = document.querySelector("#form");
 
-  form.addEventListener('submit', async (event) => {
+  form.addEventListener(
+    "submit",
+    async (event) => {
+      form.classList.add("was-validated");
+      event.preventDefault();
+      event.stopPropagation();
 
-    form.classList.add('was-validated')
-    event.preventDefault()
-    event.stopPropagation()
+      if (!form.checkValidity()) {
+        return;
+      }
+      const categoria = document.querySelector("#categorypet").value;
+      const dogName = document.querySelector("#namepet").value;
+      const tags = document.querySelector("#tagpet").value;
+      const status = document.querySelector("#statuspet").value;
+      const fotopet = document.querySelector("#fotopet").value;
+      const idpet = Math.floor(Math.random() * 10000000000000);
 
-    if (!form.checkValidity()) {
-      return;
-    }
-    const categoria = document.querySelector("#categorypet").value;
-    const dogName = document.querySelector("#namepet").value;
-    const tags = document.querySelector('#tagpet').value;
-    const status = document.querySelector('#statuspet').value;
-    const fotopet = document.querySelector('#fotopet').value;
-    const idpet = Math.floor(Math.random()*10000000000000);
-
-
-    
-
-
-
-    const pet = {
-      id: idpet,
-      category: {
-        name: categoria
-      },
-      name: dogName,
-      photoUrls: [
-        fotopet
-      ],
-      tags: [
-        {
-          name: tags
-        }
-      ],
-      status: status
-    };
-    const data = await postData('https://petstore.swagger.io/v2/pet', pet);
-    console.log(data);
-    form.submit();
-  }, false);
+      const pet = {
+        id: idpet,
+        category: {
+          name: categoria,
+        },
+        name: dogName,
+        photoUrls: [fotopet],
+        tags: [
+          {
+            name: tags,
+          },
+        ],
+        status: status,
+      };
+      const data = await postData("http://localhost:42069/pets", pet);
+      console.log(data);
+      form.submit();
+    },
+    false
+  );
 });
 
 // Ejemplo implementando el metodo POST:
-async function postData(url = '', data = {}) {
+async function postData(url = "", data = {}) {
   await fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'accept': 'application/json'
+      "Content-Type": "application/json",
+      accept: "application/json",
     },
-    body: JSON.stringify(data)
-  }).catch(error => {
-    {
-      new bootstrap.Modal(document.getElementById('exampleModal')).show();
-      console.log(error)
-    }
+    body: JSON.stringify(data),
   })
-    .then(response => response.json());
+    .catch((error) => {
+      {
+        new bootstrap.Modal(document.getElementById("exampleModal")).show();
+        console.log(error);
+      }
+    })
+    .then((response) => response.json());
 }
